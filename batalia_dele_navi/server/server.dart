b4d53a0f49fa525a38949d6f3aab5af638368279
getalSemenz <giulio.semenzato@itiszuccante.edu.it>
2023-12-02 16:46:06 +0100

    Update server.dart

--- batalia_dele_navi/server/server.dart
+++ batalia_dele_navi/server/server.dart
@@ -3,36 +3,24 @@ import 'dart:io';
 // USE also netcat 127.0.0.1 3000
 
 // global variable must be initialized later (null safety)
-late Client client;
+//late Client client;
 
 void main() {
-  ServerSocket.bind(InternetAddress.anyIPv4, 3000).then((ServerSocket server) {
-    print("server start");
-    server.listen((socket) {
-      handleConnection(socket);
-    });
-  });
+  ServerGame s = ServerGame(3000);
+  s.start();
 }
 
-void handleConnection(Socket socket) {
-  client = Client(socket);
-  print('client ' +
-      Client.N.toString() +
-      ' connected from ' +
-      '${socket.remoteAddress.address}:${socket.remotePort}');
-}
-
-void messageHandler(data) {
+void messageHandler(data, client) {
   String message = String.fromCharCodes(data).trim();
   writeMessage(client, message);
 }
 
-void errorHandler(error) {
+void errorHandler(error, client) {
   print(' Error: $error');
   client.finishedHandler();
 }
 
-void finishedHandler() {
+void finishedHandler(client) {
   client.finishedHandler();
 }
 
@@ -42,13 +30,46 @@ void writeMessage(Client client, String message) {
   client.write(str + '\n');
 }
 
-void checkMessage(Client client, String message){
+void processMessage(Client client, String message){
   String str = message.toUpperCase();
 
 }
 
-class Landpiece {
+class ServerGame {
+
+  late int _port;
+
+  List<Client> _players = [];
+
+  ServerGame(port){
+    _port = port;
+  }
   
+  void start(){
+    ServerSocket.bind(InternetAddress.anyIPv4, _port).then((ServerSocket server) {
+      print("server start");
+      server.listen((socket) {
+        handleConnection(socket);
+      });
+    });
+  }
+
+  void handleConnection(Socket socket) {
+    Client client = Client(socket);
+    _players.add(client);
+    print('client ' +
+      client._n.toString() +
+      ' connected from ' +
+      '${socket.remoteAddress.address}:${socket.remotePort}'
+    );
+  }
+
+
+
+}
+
+class Landpiece {
+
   late bool _taken;
   late bool _hitted;
 
@@ -104,18 +125,22 @@ class Landpiece {
 
 
 
+
 // the client
 
 class Client {
+
   static int N = 0;
 
   late Socket _socket;
   String get _address => _socket.remoteAddress.address;
   int get _port => _socket.remotePort;
   late int _n;
-  bool ready = false;
-  static var _client1Land = List<List>.generate(10, (i) => List<Landpiece>.generate(10, (index) => Landpiece(), growable: false), growable: false);
-  static var _client2Land = List<List>.generate(10, (i) => List<Landpiece>.generate(10, (index) => Landpiece(), growable: false), growable: false);
+  bool _ready = false;
+  bool turn = false;
+  static var _clientLand = List<List>.generate(10, (i) => List<Landpiece>.generate(10, (index) => Landpiece(), growable: false), growable: false);
+  static var _opponentLand = List<List>.generate(10, (i) => List<Landpiece>.generate(10, (index) => Landpiece(), growable: false), growable: false);
+
 
 
   Client(Socket s) {
@@ -123,11 +148,37 @@ class Client {
     _socket = s;
     _socket.listen(messageHandler,
         onError: errorHandler, onDone: finishedHandler);
+    startMessage();
   }
 
-  void messageHandler(data) {
+  void printClientLand(){
+    for(int i = 0; i < 10; i++){
+      for(int j = 0; j < 10; j++){
+        _socket.write(_clientLand[i][j]);
+      }
+      _socket.write('\n');
+    }
+  }
+
+  void printOpponentLand(){
+    for(int i = 0; i < 10; i++){
+      for(int j = 0; j < 10; j++){
+        _socket.write(_opponentLand[i][j]);
+      }
+      _socket.write('\n');
+    }
+  }
+
+  void startMessage(){
+    _socket.write('WELLCOME');
+  }
+
+  void messageHandler(data){
     String message = String.fromCharCodes(data).trim();
-    checkMessage(this, message);
+    if(_ready && turn){
+
+
+    }
   }
 
   void errorHandler(error) {
